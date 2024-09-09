ce854d820ddcc5d1be71245f839c3b1fc50fe32d
phoenixide <phoenixide@users.noreply.github.com>
2024-09-09 05:21:09 +0000

    deploy: 49ef3b96b54f4d67615270d245e69b039addbf5b

--- web-cache/3.8.9/extensionsIntegrated/DisplayShortcuts/vscode.js
+++
@@ -1,2 +0,0 @@
-define(function(require,exports,module){const KeyBindingManager=require("command/KeyBindingManager"),vscodeKeyMap=JSON.parse(require("text!./vscode.json")),VSCODE_PACK_ID="vscode";KeyBindingManager.registerCustomKeymapPack("vscode","Visual Studio Code",vscodeKeyMap)});
-//# sourceMappingURL=vscode.js.map
