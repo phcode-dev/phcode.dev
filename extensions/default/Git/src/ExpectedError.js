3a8e4c22ff2dd299e7e90066b9b36c0090fee592
phoenixide <phoenixide@users.noreply.github.com>
2025-01-31 11:08:48 +0000

    deploy: 49ef3b96b54f4d67615270d245e69b039addbf5b

---
+++ extensions/default/Git/src/ExpectedError.js
@@ -0,0 +1,2 @@
+define(function(require,exports,module){function ExpectedError(){Error.apply(this,arguments),this.message=arguments[0]}ExpectedError.prototype=new Error,ExpectedError.prototype.name="ExpectedError",ExpectedError.prototype.toString=function(){return this.message},module.exports=ExpectedError});
+//# sourceMappingURL=ExpectedError.js.map
